0c7117b70a9cff53bef857c431a76f2d9e5f78d2
Johanna Bodnyk <bodnyk@gmail.com>
2012-11-02 08:01:38 -0400

    Class work from 10/31 - starting JS

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
-Hello world!
+<!DOCTYPE html>
+<head>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.23/jquery-ui.min.js"></script>
+	<script type="text/javascript" src="js/class1.js"></script>
+	
+	<style type="text/css">
+		#lucy {
+		border: 1px solid black;
+		width: 50px;
+		height: 50px;
+		}
+	</style>
+</head>
+<body>
+
+	<div id='lucy'>
+	</div>
+	
+</body>
+</html>
